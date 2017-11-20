acb3ca683dfb01c61bcf198f6519338defa81bef
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-11-20 06:53:58 +0000

    Fix auto_ptr warnings

--- src/miner.cpp
+++ src/miner.cpp
@@ -8,6 +8,8 @@
 #include "miner.h"
 #include "kernel.h"
 
+#include <memory>
+
 using namespace std;
 
 //////////////////////////////////////////////////////////////////////////////
@@ -110,7 +112,7 @@ public:
 CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake, int64_t* pFees)
 {
     // Create new block
-    auto_ptr<CBlock> pblock(new CBlock());
+    unique_ptr<CBlock> pblock(new CBlock());
     if (!pblock.get())
         return NULL;
 
@@ -563,7 +565,7 @@ void StakeMiner(CWallet *pwallet)
         // Create new block
         //
         int64_t nFees;
-        auto_ptr<CBlock> pblock(CreateNewBlock(pwallet, true, &nFees));
+        unique_ptr<CBlock> pblock(CreateNewBlock(pwallet, true, &nFees));
         if (!pblock.get())
             return;
 
